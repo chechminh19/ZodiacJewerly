b4bcee619e357a690d644cb6b938f79fe86f0334
chechminh19 <meruem0812@gmail.com>
2024-05-20 10:45:12 +0700

    fix ProductImage
    fix

--- Domain/Entities/ProductImage.cs
+++ Domain/Entities/ProductImage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace Domain.Entities
 {
     public class ProductImage
     {
+        [Key]
+        public int Id { get; set; }
         public string? ImageUrl { get; set; }
         public string? PublicId { get; set; }      
         public int ProductId { get; set; }
