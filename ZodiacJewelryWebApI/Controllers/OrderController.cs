a569b826e7b1934260a8ce32f2afbf29ab499cfa
SameyoRoi <nguyenkyanhminh@gmail.com>
2024-07-11 16:29:10 +0700

    fix get order

--- ZodiacJewelryWebApI/Controllers/OrderController.cs
+++ ZodiacJewelryWebApI/Controllers/OrderController.cs
@@ -48,7 +48,7 @@ namespace ZodiacJewelryWebApI.Controllers
             return Ok(result);
         }
 
-        [Authorize(Roles = "Admin, Staff")]
+        [Authorize(Roles = "Staff,Admin,Customer")]
         [HttpGet]
         public async Task<IActionResult> GetAllOrders([FromQuery] int page = 1, [FromQuery] int pageSize = 5,
             [FromQuery] string search = "", [FromQuery] string filter = "", [FromQuery] string sort = "id")
