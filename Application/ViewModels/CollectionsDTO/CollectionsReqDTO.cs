3cdef8b650bd18738642e1369069ef82d7703cd7
Đinh Văn Phong <phongdvse171245@fpt.edu.vn>
2024-07-18 13:23:42 +0700

    fix

--- Application/ViewModels/CollectionsDTO/CollectionsReqDTO.cs
+++ Application/ViewModels/CollectionsDTO/CollectionsReqDTO.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
 
 namespace Application.ViewModels.CollectionsDTO;
 
 public class CollectionsReqDTO
 {
+    [Required(ErrorMessage = "Product name is required")]
+    [StringLength(100, ErrorMessage = "Product name can't be longer than 100 characters")]
     public string? NameCollection { get; set; }
     public IFormFile? ImageCollection { get; set; } 
     public DateTime DateClose { get; set; }
